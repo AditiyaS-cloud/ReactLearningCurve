d7826f195210711148fd1d53fd22697b756fe32a
Aditiya Srivastava <aditiyasrivastava1996@gmail.com>
2023-01-14 21:41:11 +0530

    changes

--- App.js
+++ App.js
@@ -1,32 +1,23 @@
 import React from "react";
-import ReactDOM  from "react-dom/client";
+import ReactDOM from "react-dom/client";
 
-const heading = React.createElement(
-    "h1",
-    {
-      id: "title",
-    },
-    "React Learning Curve"
+const Title = () => (
+  <h1 id="title" key="h2">
+    Helllooo!!
+  </h1>
+)
+
+//Composing Components
+const HeaderComponent = () => {
+  return (
+    <div>
+      <Title />
+      <h2>This is a h2 tag</h2>
+    </div>
   );
-  const heading2 = React.createElement(
-    "h2",
-    {
-      id: "title",
-    },
-    "First Project- Basic concepts!!"
-  );
-  const container = React.createElement(
-    "div",
-    {
-      id: "container",
-    },
-    //this is an array
-    [heading, heading2]
-  );
+};
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
+//passing a react element inside the root
 
-  const root = ReactDOM.createRoot(document.getElementById("root"));
-
-  //passing a react element inside the root
-  
-  root.render(container);
+root.render(<HeaderComponent />);
