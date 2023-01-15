dc2689f61fe1c6ad7443f0dcb2449dc95166ecc4
Aditiya Srivastava <aditiyasrivastava1996@gmail.com>
2023-01-15 11:09:52 +0530

    Added components

---
+++ constants.js
@@ -0,0 +1,3 @@
+export const IMG_CDN_URL = 'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/';
+
+export const RESTAURANT_API = 'https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.9940437&lng=72.5277471&page_type=DESKTOP_WEB_LISTING';
---
+++ src/components/Body.js
@@ -0,0 +1,70 @@
+import RestaurantCard from "./RestaurantCard";
+import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
+import { RESTAURANT_API } from "../../constants";
+
+function filterData(searchText, restaurants) {
+  return restaurants.filter((x) => {
+    return x?.data?.name?.toLowerCase().includes(searchText.toLowerCase());
+  });
+}
+
+const Body = () => {
+  const [allRestaurants, setAllRestaurants] = useState([]);
+  const [filteredRestaurants, setFilteredRestaurants] = useState([]);
+  const [searchText, setSearchText] = useState("");
+
+  useEffect(() => {
+    getRestaurantList();
+  }, []);
+
+  async function getRestaurantList() {
+    try {
+      let data = await fetch(RESTAURANT_API);
+
+      let fetchJson = await data.json();
+
+      setAllRestaurants(fetchJson?.data?.cards[2]?.data?.data?.cards);
+      setFilteredRestaurants(fetchJson?.data?.cards[2]?.data?.data?.cards);
+    } catch (error) {
+      console.log("There was an error while fetching restaurant ddata", error);
+    }
+  }
+
+  return allRestaurants?.length === 0 ? (
+    <Shimmer />
+  ) : (
+    <>
+      <div className="search-container">
+        <input
+          type="text"
+          className="search-input"
+          placeholder="search"
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}
+        />
+        <button
+          className="search-btn"
+          onClick={() => {
+            const data = filterData(searchText, allRestaurants);
+            setFilteredRestaurants(data);
+          }}
+        >
+          {" "}
+          Search
+        </button>
+      </div>
+      <div className="restaurant-list">
+        {filteredRestaurants.length === 0
+          ? "No data found"
+          : filteredRestaurants.map((restaurant) => {
+              return (
+                <RestaurantCard {...restaurant.data} key={restaurant.data.id} />
+              );
+            })}
+      </div>
+    </>
+  );
+};
+
+export default Body;
